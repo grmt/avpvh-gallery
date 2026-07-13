bbc9735160e7f033539d990d1af5343e460e45f6
grmt <garmt.noname@gmail.com>
2026-07-13 21:02:46 +0200

    deploy: bump version to 2.13.14.16

--- src/php/avpvh-gallery.php
+++ src/php/avpvh-gallery.php
@@ -11,7 +11,7 @@
 Plugin Name:       AVPVH Gallery
 Plugin URI:        https://github.com/grmt/avpvh-gallery
 Description:       A WordPress gallery using Google Drive as file storage
-Version:           2.13.14.15
+Version:           2.13.14.16
 Requires at least: 6.5
 Requires PHP:      8.1
 Author:            Garmt Boekholt (info@avphilipsvanhorne.nl), based on work by Junák - český skaut
